d833153c15361318576b7d5417395b62faac30b3
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-11-01 12:21:10 -0700

    channel service

--- concurrency/main.go
+++ concurrency/main.go
@@ -30,26 +30,13 @@ func (s *atomicIdService) getNext() uint64 {
 	return s.counter
 }
 
-type mutexIdService struct {
-	counter uint64
-}
-
-func (s *mutexIdService) getNext() uint64 {
-	mu.Lock() {
-		s.counter += 1
-	}
-	mu.Unlock()
-
-	return s.counter
-}
-
 func run(wg *sync.WaitGroup, s idService, label string) {
 	go func() {
 		for i := 0; i < 99999; i++ {
 			s.getNext()
 		}
 
-		fmt.Printf("%s 1:%d\n", label, s.getNext())
+		fmt.Printf("%s  1:%d\n", label, s.getNext())
 		wg.Done()
 	}()
 
@@ -58,22 +45,103 @@ func run(wg *sync.WaitGroup, s idService, label string) {
 			s.getNext()
 		}
 
-		fmt.Printf("%s 2:%d\n", label, s.getNext())
+		fmt.Printf("%s  2:%d\n", label, s.getNext())
 		wg.Done()
 	}()
 }
 
+func runWithMutex(wg *sync.WaitGroup, mu *sync.Mutex, s idService) {
+	var c uint64
+
+	go func() {
+		for i := 0; i < 99999; i++ {
+			mu.Lock()
+			{
+				s.getNext()
+			}
+			mu.Unlock()
+		}
+
+		mu.Lock()
+		{
+			c = s.getNext()
+		}
+		mu.Unlock()
+
+		fmt.Printf("mutex   1:%d\n", c)
+		wg.Done()
+	}()
+
+	go func() {
+		for i := 0; i < 99999; i++ {
+			mu.Lock()
+			{
+				s.getNext()
+			}
+			mu.Unlock()
+		}
+
+		mu.Lock()
+		{
+			c = s.getNext()
+		}
+		mu.Unlock()
+
+		fmt.Printf("mutex   2:%d\n", c)
+		wg.Done()
+	}()
+}
+
+func runWithChannels(wg *sync.WaitGroup, s idService) {
+	ch1 := make(chan uint64)
+	ch2 := make(chan uint64)
+
+	go func() {
+		for i := 0; i < 100000; i++ {
+			ch1 <- s.getNext()
+		}
+
+		wg.Done()
+	}()
+
+	go func() {
+		for i := 0; i < 100000; i++ {
+			ch2 <- s.getNext()
+		}
+
+		wg.Done()
+	}()
+
+	var c uint64
+
+	for i := 0; i < 100000; i++ {
+		c = <-ch1
+	}
+
+	fmt.Printf("channel 1:%d\n", c)
+
+	for i := 0; i < 100000; i++ {
+		c = <-ch2
+	}
+
+	fmt.Printf("channel 2:%d\n", c)
+}
+
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(6)
+	wg.Add(8)
+
+	var mu sync.Mutex
 
 	s1 := &basicIdService{}
 	s2 := &atomicIdService{}
-	s3 := &mutexIdService{}
+	s3 := &basicIdService{}
+	s4 := &basicIdService{}
 
 	run(&wg, s1, "basic ")
 	run(&wg, s2, "atomic")
-	run(&wg, s2, "mutex ")
+	runWithMutex(&wg, &mu, s3)
+	runWithChannels(&wg, s4)
 
 	fmt.Println("Waiting to finish...")
 	wg.Wait()
