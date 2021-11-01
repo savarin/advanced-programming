e80d3b51e4277d6f86ad42c5ac4578276ee513e9
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-11-01 11:35:41 -0700

    atomic service

--- concurrency/main.go
+++ concurrency/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 type idService interface {
@@ -20,30 +21,44 @@ func (s *basicIdService) getNext() uint64 {
 	return s.counter
 }
 
+type atomicIdService struct {
+	counter uint64
+}
+
+func (s *atomicIdService) getNext() uint64 {
+	atomic.AddUint64(&s.counter, 1)
+	return s.counter
+}
+
+func run(wg *sync.WaitGroup, s idService, label string) {
+	go func() {
+		for i := 0; i < 99999; i++ {
+			s.getNext()
+		}
+
+		fmt.Printf("%s 1:%d\n", label, s.getNext())
+		wg.Done()
+	}()
+
+	go func() {
+		for i := 0; i < 99999; i++ {
+			s.getNext()
+		}
+
+		fmt.Printf("%s 2:%d\n", label, s.getNext())
+		wg.Done()
+	}()
+}
+
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(4)
 
-	s := &basicIdService{}
+	s1 := &basicIdService{}
+	s2 := &atomicIdService{}
 
-	go func() {
-		for i := 0; i < 1000; i++ {
-			s.getNext()
-		}
-
-		fmt.Println("goroutine 1:", s.counter)
-		wg.Done()
-	}()
-
-	go func() {
-		for i := 0; i < 1000; i++ {
-			s.getNext()
-		}
-
-		fmt.Println("goroutine 2:", s.counter)
-
-		wg.Done()
-	}()
+	run(&wg, s1, "basic")
+	run(&wg, s2, "atomic")
 
 	fmt.Println("Waiting to finish...")
 	wg.Wait()
