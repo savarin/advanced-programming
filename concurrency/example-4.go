e83f10a88a75659ee5638228e679d115e6bea92e
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-11-02 16:46:25 -0700

    solve example-4

--- concurrency/example-4.go
+++ concurrency/example-4.go
@@ -8,9 +8,9 @@ func main() {
 	done := make(chan struct{}, 1)
 	go func() {
 		fmt.Println("performing initialization...")
-		<-done
+		done <- struct{}{}		
 	}()
 
-	done <- struct{}{}
+	<-done
 	fmt.Println("initialization done, continuing with rest of program")
 }
