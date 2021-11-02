0a242ca25171cf589ded4c699bba08198cb6ddc9
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-11-02 16:31:28 -0700

    solve example-1

--- concurrency/example-1.go
+++ concurrency/example-1.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	for i := 0; i < 10; i++ {
-		go func() {
+		go func(i int) {
 			fmt.Printf("launched goroutine %d\n", i)
-		}()
+		}(i)
 	}
 	// Wait for goroutines to finish
 	time.Sleep(time.Second)
